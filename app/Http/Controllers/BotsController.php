591455f73eb9135857b949b2939a33d5c653a6b2
fernandosg <fsg_tmp@hotmail.com>
2017-07-29 02:15:38 -0500

    Using the FbMessenger wrapper class in the endpoint talk in BotsController.

--- app/Http/Controllers/BotsController.php
+++ app/Http/Controllers/BotsController.php
@@ -1,5 +1,6 @@
 <?php namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 class BotsController extends Controller {
 
     const MODEL = "App\Chats";
@@ -8,6 +9,19 @@ class BotsController extends Controller {
     use RESTActions;
     /* Endpoint for getting the messages from webhook call*/
     public function talk(Request $request){
-      
+      $m = self::FbMessenger;
+      $accesstoken=env('ACCESS_TOKEN', "");
+      $page_id=693777000809331;//YOUR PAGE_ID
+      $msn=new $m($accesstoken,$page_id,json_decode(file_get_contents('php://input'), true));
+      $msn->init();
+      if($msn->isAPostback()){
+        if($msn->isPostbackLike("ACCEPTING")){
+          $msn->sendMessage("Thanks for accepting");
+        }else if($msn->isPostbackLike("NOT_ACCEPTING")){
+          $msn->sendMessage("Maybe the next time =/");
+        }
+      }else{
+        $msn->sendMessageWithMultipleButtons("This is the message that should be displayed in the box of multiple button selection",array(array("type"=>"postback","title"=>"I am agree","payload"=>"ACCEPTING"),array("type"=>"postback","title"=>"I am not agree","payload"=>"NOT_ACCEPTING")));
+      }
     }
 }
