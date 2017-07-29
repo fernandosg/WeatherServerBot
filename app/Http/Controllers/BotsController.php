a98f439c80629561d64c63a5764f6a21d75f9bdf
fernandosg <fsg_tmp@hotmail.com>
2017-07-29 17:34:40 -0500

    Refactoring. Checking if the talk is not yet concluded, then should be fire the conversation where the user left.

--- app/Http/Controllers/BotsController.php
+++ app/Http/Controllers/BotsController.php
@@ -17,20 +17,31 @@ class BotsController extends Controller {
       $msn=new $m($accesstoken,$page_id,json_decode(file_get_contents('php://input'), true));
       $msn->init();
       $BotCommunication=new $bot($msn->getSender());
+      $last_talk=$BotCommunication->isSomeTalkNotClosed();
       if($msn->isAPostback()){
         if($msn->isPostbackLike("ACCEPTING")){
-          $this->ACCEPTING($msn,$BotCommunication);
+          if($last_talk["is_closed"])
+            $this->ACCEPTING($msn,$BotCommunication);
+          else
+            $this->fireNotClosedTalk($last_talk["last_message"],$msn,$BotCommunication);
         }else if($msn->isPostbackLike("NOT_ACCEPTING")){
+          if($last_talk["is_closed"])
+            $this->NOT_ACCEPTING($msn,$BotCommunication);
+          else
+            $this->fireNotClosedTalk($last_talk["last_message"],$msn,$BotCommunication);
+        }else if($msn->isPostbackLike("CONTINUE_TALKING")){
+          $this->CONTINUE_TALKING($msn,$BotCommunication);
+        }else if($msn->isPostbackLike("STOP_TALKING")){
           $this->NOT_ACCEPTING($msn,$BotCommunication);
         }
-      }else{
+      }else if($msn->isTheUser()){
         $this->sendMessageWithMultipleButtons($msn,$BotCommunication);
       }
     }
 
     public function sendMessageWithMultipleButtons($msn,$BotCommunication){
-        $msn->sendMessageWithMultipleButtons("This is the message that should be displayed in the box of multiple button selection",array(array("type"=>"postback","title"=>"I am agree","payload"=>"ACCEPTING"),array("type"=>"postback","title"=>"I am not agree","payload"=>"NOT_ACCEPTING")));
-        $BotCommunication->saveCommunication(1,false);
+        $msn->sendMessageWithMultipleButtons("This is the message that should be displayed in the box of multiple button selection",array(array("type"=>"postback","title"=>"I am agree","payload"=>"ACCEPTING"),array("type"=>"postback","title"=>"I am not agree","payload"=>"NOT_ACCEPTING"),array("type"=>"postback","title"=>"Lets continue talking","payload"=>"CONTINUE_TALKING")));
+        $BotCommunication->saveCommunication(1,true);
     }
 
     public function NOT_ACCEPTING($msn,$BotCommunication){
@@ -41,5 +52,22 @@ class BotsController extends Controller {
     public function ACCEPTING($msn,$BotCommunication){
       $msn->sendMessage("Thanks for accepting");
       $BotCommunication->saveCommunication(2,true);
+    }
+
+    public function CONTINUE_TALKING($msn,$BotCommunication){
+        $msn->sendMessage("Lets continue talking");
+        $msn->sendMessageWithMultipleButtons("This is the message that should be displayed in the box of multiple button selection",array(array("type"=>"postback","title"=>"I dont want, sorry.","payload"=>"STOP_TALKING"),array("type"=>"postback","title"=>"I dont like the idea","payload"=>"STOP_TALKING")));
+        $BotCommunication->saveCommunication(4,false);
+    }
+
+    public function fireNotClosedTalk($talk,$msn,$BotCommunication){
+      switch ($talk) {
+        case 4:
+          $msn->sendMessage("Mmmm you forgot to conclude the talk");
+          $this->CONTINUE_TALKING($msn,$BotCommunication);
+          break;
+        default:
+          break;
+      }
     }
 }
