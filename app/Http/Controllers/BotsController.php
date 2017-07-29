808b6ebfe334313ef3971d67521c21bd7985904c
fernandosg <fsg_tmp@hotmail.com>
2017-07-29 14:23:07 -0500

    Implementing BotCommunication class in BotsController.

--- app/Http/Controllers/BotsController.php
+++ app/Http/Controllers/BotsController.php
@@ -5,20 +5,25 @@ class BotsController extends Controller {
 
     const MODEL = "App\Chats";
     const FbMessenger = "App\Messenger";
+    const BotCommunication = "App\BotCommunication";
 
     use RESTActions;
     /* Endpoint for getting the messages from webhook call*/
     public function talk(Request $request){
       $m = self::FbMessenger;
+      $bot=self::BotCommunication;
       $accesstoken=env('ACCESS_TOKEN', "");
       $page_id=693777000809331;//YOUR PAGE_ID
       $msn=new $m($accesstoken,$page_id,json_decode(file_get_contents('php://input'), true));
       $msn->init();
+      $BotCommunication=new $bot($msn->getSender());
       if($msn->isAPostback()){
         if($msn->isPostbackLike("ACCEPTING")){
           $msn->sendMessage("Thanks for accepting");
+          $BotCommunication->saveCommunication(2);
         }else if($msn->isPostbackLike("NOT_ACCEPTING")){
           $msn->sendMessage("Maybe the next time =/");
+          $BotCommunication->saveCommunication(3);
         }
       }else{
         $msn->sendMessageWithMultipleButtons("This is the message that should be displayed in the box of multiple button selection",array(array("type"=>"postback","title"=>"I am agree","payload"=>"ACCEPTING"),array("type"=>"postback","title"=>"I am not agree","payload"=>"NOT_ACCEPTING")));
