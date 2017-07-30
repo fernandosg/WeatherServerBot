84781d248fe33aa29b7b9a91ebca711718742e80
fernandosg <fsg_tmp@hotmail.com>
2017-07-29 19:00:50 -0500

    Refactoring. Implemented the logic of weather (a simple test).

--- app/Http/Controllers/BotsController.php
+++ app/Http/Controllers/BotsController.php
@@ -3,72 +3,66 @@
 use Illuminate\Http\Request;
 class BotsController extends Controller {
 
-    const MODEL = "App\Chats";
-    const FbMessenger = "App\Messenger";
-    const BotCommunication = "App\BotCommunication";
-    var $msn;
-    var $BotCommunication;
-    use RESTActions;
-    /* Endpoint for getting the messages from webhook call*/
-    public function talk(Request $request){
-      $m = self::FbMessenger;
-      $bot=self::BotCommunication;
-      $accesstoken=env('ACCESS_TOKEN', "");
-      $page_id=693777000809331;//YOUR PAGE_ID
-      $this->msn=new $m($accesstoken,$page_id,json_decode(file_get_contents('php://input'), true));
-      $this->msn->init();
-      $this->BotCommunication=new $bot($this->msn->getSender());
-      $last_talk=$this->BotCommunication->isSomeTalkNotClosed();
-      if($this->msn->isAPostback()){
-        if($this->msn->isPostbackLike("ACCEPTING")){
-          if($last_talk["is_closed"])
-            $this->ACCEPTING();
-          else
-            $this->fireNotClosedTalk($last_talk["last_message"]);
-        }else if($this->msn->isPostbackLike("NOT_ACCEPTING")){
-          if($last_talk["is_closed"])
-            $this->NOT_ACCEPTING();
-          else
-            $this->fireNotClosedTalk($last_talk["last_message"]);
-        }else if($this->msn->isPostbackLike("CONTINUE_TALKING")){
-          $this->CONTINUE_TALKING();
-        }else if($this->msn->isPostbackLike("STOP_TALKING")){
-          $this->NOT_ACCEPTING();
-        }
-      }else if($this->msn->isTheUser()){
-        $this->sendMessageWithMultipleButtons();
+  const MODEL = "App\Chats";
+  const FbMessenger = "App\Messenger";
+  const BotCommunication = "App\BotCommunication";
+  var $msn;
+  var $BotCommunication;
+  use RESTActions;
+  /* Endpoint for getting the messages from webhook call*/
+  public function talk(Request $request){
+    $m = self::FbMessenger;
+    $bot=self::BotCommunication;
+    $accesstoken=env('ACCESS_TOKEN', "");
+    $page_id=693777000809331;//YOUR PAGE_ID
+    $this->msn=new $m($accesstoken,$page_id,json_decode(file_get_contents('php://input'), true));
+    $this->msn->init();
+    $this->BotCommunication=new $bot($this->msn->getSender());
+    $last_talk=$this->BotCommunication->isSomeTalkNotClosed();
+    if($this->msn->isAPostback()){
+      if($this->msn->isPostbackLike("TAMPICO_WEATHER_DELIVERING")){
+        if($last_talk["is_closed"])
+          $this->sendWeatherMessage("Tampico",1,true);
+        else
+          $this->fireNotClosedTalk($last_talk["last_message"]);
+      }else if($this->msn->isPostbackLike("CDMADERO_WEATHER_DELIVERING")){
+        if($last_talk["is_closed"])
+          $this->sendWeatherMessage("Ciudad Madero",2,true);
+        else
+          $this->fireNotClosedTalk($last_talk["last_message"]);
+      }else if($this->msn->isPostbackLike("ALTAMIRA_WEATHER_DELIVERING")){
+        $this->sendWeatherMessage("Altamira",3,true);
+      }else if($this->msn->isPostbackLike("STOP_TALKING")){
+        $this->NOT_ACCEPTING();
       }
+    }else if($this->msn->isTheUser() && $this->msn->getMessage()=="Hi"){
+      $this->sendMessageWithMultipleButtons();
+    }else{
+      $this->msn->sendMessage("I dont understand sorry =/, ¿do you want something? please say Hi.");
     }
+  }
 
-    public function sendMessageWithMultipleButtons(){
-        $this->msn->sendMessageWithMultipleButtons("This is the message that should be displayed in the box of multiple button selection",array(array("type"=>"postback","title"=>"I am agree","payload"=>"ACCEPTING"),array("type"=>"postback","title"=>"I am not agree","payload"=>"NOT_ACCEPTING"),array("type"=>"postback","title"=>"Lets continue talking","payload"=>"CONTINUE_TALKING")));
-        $this->BotCommunication->saveCommunication(1,true);
-    }
+  public function sendMessageWithMultipleButtons(){
+    $this->msn->sendMessageWithMultipleButtons("Hi my name is Bob, this is a test for checking the API FbMessenger, my function is to deliver to you the weather in some local cities from México, ¿what city do you select? ",array(array("type"=>"postback","title"=>"Tampico","payload"=>"TAMPICO_WEATHER_DELIVERING"),array("type"=>"postback","title"=>"Cd Madero","payload"=>"CDMADERO_WEATHER_DELIVERING"),array("type"=>"postback","title"=>"Altamira","payload"=>"ALTAMIRA_WEATHER_DELIVERING")));
+    $this->BotCommunication->saveCommunication(1,true);
+  }
 
-    public function NOT_ACCEPTING(){
-        $this->msn->sendMessage("Maybe the next time =/");
-        $this->BotCommunication->saveCommunication(3,true);
-    }
 
-    public function ACCEPTING(){
-      $this->msn->sendMessage("Thanks for accepting");
-      $this->BotCommunication->saveCommunication(2,true);
-    }
+  public function sendWeatherMessage($city,$option,$is_conclude){
+    $this->msn->sendMessage("The ".$city." Weather is");
+    $this->BotCommunication->saveCommunication($option,$is_conclude);
+  }
 
-    public function CONTINUE_TALKING(){
-        $this->msn->sendMessage("Lets continue talking");
-        $this->msn->sendMessageWithMultipleButtons("This is the message that should be displayed in the box of multiple button selection",array(array("type"=>"postback","title"=>"I dont want, sorry.","payload"=>"STOP_TALKING"),array("type"=>"postback","title"=>"I dont like the idea","payload"=>"STOP_TALKING")));
-        $this->BotCommunication->saveCommunication(4,false);
-    }
 
-    public function fireNotClosedTalk($talk){
-      switch ($talk) {
-        case 4:
-          $this->msn->sendMessage("Mmmm you forgot to conclude the talk");
-          $this->CONTINUE_TALKING();
-          break;
-        default:
-          break;
-      }
+
+  public function fireNotClosedTalk($talk){
+    switch ($talk) {
+      case 4:
+        $this->msn->sendMessage("Mmmm you forgot to conclude the talk");
+        // In each city should be display the message for receive the date.
+      break;
+      default:
+      break;
     }
+  }
 }
