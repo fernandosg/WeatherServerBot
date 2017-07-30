0813fb860e308b8e30ea52caca731b3b5aa7e6da
fernandosg <fsg_tmp@hotmail.com>
2017-07-29 21:32:12 -0500

    Refactoring. Instead of the ifelse nightmare that i had before, now in the postback is added the name of the city and other string indication that help when the server receives the message, and defines which action (the response that should be sent to the user) should be launched.

--- app/Http/Controllers/BotsController.php
+++ app/Http/Controllers/BotsController.php
@@ -20,48 +20,48 @@ class BotsController extends Controller {
     $this->BotCommunication=new $bot($this->msn->getSender());
     $last_talk=$this->BotCommunication->isSomeTalkNotClosed();
     if($this->msn->isAPostback()){
-      if($this->msn->isPostbackLike("TAMPICO_WEATHER_DELIVERING")){
-        if($last_talk["is_closed"])
-          $this->sendWeatherMessage("Tampico",1,false);
-        else
-          $this->fireNotClosedTalk($last_talk["last_message"]);
-      }else if($this->msn->isPostbackLike("CDMADERO_WEATHER_DELIVERING")){
-        if($last_talk["is_closed"])
-          $this->sendWeatherMessage("Ciudad Madero",2,false);
-        else
-          $this->fireNotClosedTalk($last_talk["last_message"]);
-      }else if($this->msn->isPostbackLike("ALTAMIRA_WEATHER_DELIVERING")){
-        $this->sendWeatherMessage("Altamira",3,false);
-      }else if($this->msn->isPostbackLike("STOP_TALKING")){
-        $this->NOT_ACCEPTING();
-      }else if($this->msn->isPostbackLike("TODAY_WEATHER")){
-        $this->msn->sendMessage("The weather today is ");
-      }else if($this->msn->isPostbackLike("TOMORROW_WEATHER")){
-        $this->msn->sendMessage("The weather tomorrow is");
+      $postback=$this->msn->getPostback();
+      if(strpos($postback,"WEATHER")!==false){
+          $information=explode("_",$postback);
+          if(sizeof($information)>=3){
+              $this->msn->sendMessage("The weather ".$information[1]." in ".$information[0]." is");
+              $this->BotCommunication->saveCommunication($this->getCityIndex($information[0])+5,true);// VER COMO PONERLE EL INDICE
+          }else if(sizeof($information)==2){
+            $this->sendWeatherMessage($information[0],$this->getCityIndex($information[0]),false);// VER COMO PONERLE EL INDICE
+          }
       }
     }else if($this->msn->isTheUser() && $this->msn->getMessage()=="Hi"){
-      $this->sendMessageWithMultipleButtons();
+      if($last_talk["is_closed"]){
+        $this->sendMessageWithMultipleButtons();
+      }else{
+        $this->msn->sendMessage("¿Is there a talk that ends?");
+      }
+      /*else This should be fired soon.
+        $this->fireNotClosedTalk($last_talk["last_message"]);
+        */
     }else{
       $this->msn->sendMessage("I dont understand sorry =/, ¿do you want something? please say Hi.");
     }
   }
 
   public function sendMessageWithMultipleButtons(){
-    $this->msn->sendMessageWithMultipleButtons("Hi my name is Bob, this is a test for checking the API FbMessenger, my function is to deliver to you the weather in some local cities from México, ¿what city do you select? ",array(array("type"=>"postback","title"=>"Tampico","payload"=>"TAMPICO_WEATHER_DELIVERING"),array("type"=>"postback","title"=>"Cd Madero","payload"=>"CDMADERO_WEATHER_DELIVERING"),array("type"=>"postback","title"=>"Altamira","payload"=>"ALTAMIRA_WEATHER_DELIVERING")));
+    $this->msn->sendMessageWithMultipleButtons("Hi my name is Bob, this is a test for checking the API FbMessenger, my function is to deliver to you the weather in some local cities from México, ¿what city do you select? ",array(array("type"=>"postback","title"=>"Tampico","payload"=>"TAMPICO_WEATHER"),array("type"=>"postback","title"=>"Cd Madero","payload"=>"CDMADERO_WEATHER"),array("type"=>"postback","title"=>"Altamira","payload"=>"ALTAMIRA_WEATHER")));
     $this->BotCommunication->saveCommunication(1,true);
   }
 
-  public function sendMessageForDays(){
-    $this->msn->sendMessageWithMultipleButtons("¡Great!, ¿which day do you want to know the weather? ",array(array("type"=>"postback","title"=>"Today","payload"=>"TODAY_WEATHER"),array("type"=>"postback","title"=>"Tomorrow","payload"=>"TOMORROW_WEATHER")));
-    $this->BotCommunication->saveCommunication(4,false);
+  public function sendMessageForDays($city,$option){
+    $this->msn->sendMessageWithMultipleButtons("¡Great!, ¿which day do you want to know the weather? ",array(array("type"=>"postback","title"=>"Today","payload"=>($city."_TODAY_WEATHER")),array("type"=>"postback","title"=>"Tomorrow","payload"=>($city."_TOMORROW_WEATHER"))));
+    $this->BotCommunication->saveCommunication($option,false);
   }
 
   public function sendWeatherMessage($city,$option,$is_conclude){
-    $this->sendMessageForDays();
-    $this->BotCommunication->saveCommunication($option,$is_conclude);
+    $this->sendMessageForDays($city,$option);
   }
 
-
+  public function getCityIndex($city){
+    $array=array("TAMPICO","CDMADERO","ALTAMIRA");
+    return (array_search($city,$array)+2);
+  }
 
   public function fireNotClosedTalk($talk){
     switch ($talk) {
