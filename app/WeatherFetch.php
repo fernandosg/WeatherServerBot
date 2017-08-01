bcb0387ab3066efb18e640d29fcfb10418e3b71f
fernandosg <fsg_tmp@hotmail.com>
2017-07-31 19:24:28 -0500

    Implemented method for checking the weather in some day

--- app/WeatherFetch.php
+++ app/WeatherFetch.php
@@ -8,5 +8,16 @@
       $response=curl_exec($handler);
       return json_decode($response);
     }
+
+    public static function getTemperatureByCityOnDay($city_name,$APIKEY,$day){
+      if($day=="TODAY")
+        return WeatherFetch::getTemperatureByCity($city_name,$APIKEY);
+      $path="api.openweathermap.org/data/2.5/forecast?q=".$city_name."&APPID=".$APIKEY."&units=metric&cnt=5";
+      $handler=curl_init($path);
+      curl_setopt($handler,CURLOPT_RETURNTRANSFER,1);
+      $response=curl_exec($handler);
+      $information=json_decode($response);
+      return $information->list[1];
+    }
   }
 ?>
